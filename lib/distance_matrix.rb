2fd740e53d33ab40eff9b9f98833d3ce3b208b18
Ben J Woodcroft <donttrustben near gmail.com>
2009-01-12 16:26:13 +1000

    more testing of distance_matrix.rb complete. Fairly happy with it now, excepting error correction

--- lib/distance_matrix.rb
+++ lib/distance_matrix.rb
@@ -1,6 +1,38 @@
 # A class to represent a distance matrix like BLOSUM62
 
 class DistanceMatrix
+  
+  BLOSUM62 = <<END_OF_BLOSUM62
+# Entries for the BLOSUM62 matrix at a scale of ln(2)/2.0.
+   A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  J  Z  X  *
+A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1 -1 -1 -4
+R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1 -2  0 -1 -4
+N -2  0  6  1 -3  0  0  0  1 -3 -3  0 -2 -3 -2  1  0 -4 -2 -3  4 -3  0 -1 -4
+D -2 -2  1  6 -3  0  2 -1 -1 -3 -4 -1 -3 -3 -1  0 -1 -4 -3 -3  4 -3  1 -1 -4
+C  0 -3 -3 -3  9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -1 -3 -1 -4
+Q -1  1  0  0 -3  5  2 -2  0 -3 -2  1  0 -3 -1  0 -1 -2 -1 -2  0 -2  4 -1 -4
+E -1  0  0  2 -4  2  5 -2  0 -3 -3  1 -2 -3 -1  0 -1 -3 -2 -2  1 -3  4 -1 -4
+G  0 -2  0 -1 -3 -2 -2  6 -2 -4 -4 -2 -3 -3 -2  0 -2 -2 -3 -3 -1 -4 -2 -1 -4
+H -2  0  1 -1 -3  0  0 -2  8 -3 -3 -1 -2 -1 -2 -1 -2 -2  2 -3  0 -3  0 -1 -4
+I -1 -3 -3 -3 -1 -3 -3 -4 -3  4  2 -3  1  0 -3 -2 -1 -3 -1  3 -3  3 -3 -1 -4
+L -1 -2 -3 -4 -1 -2 -3 -4 -3  2  4 -2  2  0 -3 -2 -1 -2 -1  1 -4  3 -3 -1 -4
+K -1  2  0 -1 -3  1  1 -2 -1 -3 -2  5 -1 -3 -1  0 -1 -3 -2 -2  0 -3  1 -1 -4
+M -1 -1 -2 -3 -1  0 -2 -3 -2  1  2 -1  5  0 -2 -1 -1 -1 -1  1 -3  2 -1 -1 -4
+F -2 -3 -3 -3 -2 -3 -3 -3 -1  0  0 -3  0  6 -4 -2 -2  1  3 -1 -3  0 -3 -1 -4
+P -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4  7 -1 -1 -4 -3 -2 -2 -3 -1 -1 -4
+S  1 -1  1  0 -1  0  0  0 -1 -2 -2  0 -1 -2 -1  4  1 -3 -2 -2  0 -2  0 -1 -4
+T  0 -1  0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1  1  5 -2 -2  0 -1 -1 -1 -1 -4
+W -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1  1 -4 -3 -2 11  2 -3 -4 -2 -2 -1 -4
+Y -2 -2 -2 -3 -2 -1 -2 -3  2 -1 -1 -2 -1  3 -3 -2 -2  2  7 -1 -3 -1 -2 -1 -4
+V  0 -3 -3 -3 -1 -2 -2 -3 -3  3  1 -2  1 -1 -2 -2  0 -3 -1  4 -3  2 -2 -1 -4
+B -2 -1  4  4 -3  0  1 -1  0 -3 -4  0 -3 -3 -2  0 -1 -4 -3 -3  4 -3  0 -1 -4
+J -1 -2 -3 -3 -1 -2 -3 -4 -3  3  3 -3  2  0 -3 -2 -1 -2 -1  2 -3  3 -3 -1 -4
+Z -1  0  0  1 -3  4  4 -2  0 -3 -3  1 -1 -3 -1  0 -1 -2 -2 -2  0 -3  4 -1 -4
+X -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -4
+* -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4  1
+END_OF_BLOSUM62
+  
+  
   def load_matrix(io)
     column_names = []
     @matrix_hash = {}
@@ -14,14 +46,23 @@ class DistanceMatrix
         splut = line.strip.split
         r = {}
         column_names.each_with_index do |col, index|
-          r[col] = splut[index+1]
+          r[col] = splut[index+1].to_i
         end
         @matrix_hash[splut[0]] = r
       end
     end
+    
+    # return self for convenience
+    self
   end
   
   def get(column_letter, row_letter)
+    # For most matrices the ordering of row, col doesn't matter.
     @matrix_hash[row_letter][column_letter]
+  end
+  
+  def self.blosum62
+    d = self.new
+    d.load_matrix(BLOSUM62) #load_matrix returns the created distance matrix
   end
 end
--- lib/distance_matrix_test.rb
+++ lib/distance_matrix_test.rb
@@ -22,4 +22,26 @@ __END_OF_TESTDATA__
     assert_equal 2, d.get('B','A')
     assert_equal 4, d.get('B','B')
   end
+  
+  def test_negative_and_comments
+    input = <<__END_OF_TESTDATA__
+   A  B  C
+A  1  2 -1
+B  3  4 -2
+C  5  6  8
+//
+__END_OF_TESTDATA__
+    d = DistanceMatrix.new
+    d.load_matrix(input)
+    assert_equal 1, d.get('A','A')
+    assert_equal 2, d.get('B','A')
+    assert_equal 4, d.get('B','B')
+    assert_equal(-1, d.get('C','A'))
+  end
+  
+  def test_blosum62
+    d = DistanceMatrix.blosum62
+    assert_equal 4, d.get('A','A')
+    assert_equal -1, d.get('A','R')
+  end
 end
