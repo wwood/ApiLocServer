18eed9abc02e695864e52bcb379d6f8496e2e8d9
Ben J Woodcroft <donttrustben near gmail.com>
2009-01-18 16:56:39 +1000

    added possible routing fix for apiloc production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,3 +19,7 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Route apiloc properly. Without this I get routing errors with rails 2.2.2
+# http://groups.google.com/group/phusion-passenger/browse_thread/thread/bc1c7e99701ab5db
+config.action_controller.relative_url_root = "/apiloc"
