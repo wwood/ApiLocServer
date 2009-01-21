18ae06cbe88e4754ac407b2e6c4590e3db2d30cf
Ben J Woodcroft <donttrustben near gmail.com>
2009-01-21 11:40:00 +1000

    added percentile support to microarrays

--- app/models/microarray_measurement.rb
+++ app/models/microarray_measurement.rb
@@ -16,4 +16,24 @@ class MicroarrayMeasurement < ActiveRecord::Base
       :conditions => ["microarray_timepoints.name in ('#{timepoint_names_array.join("','")}')"]
     }
   }
+  
+    
+  # The percentile is what percentage of measurements in this class does this
+  # measurement beat?
+  def percentile
+    nil if measurement.nil?
+    
+    belows = MicroarrayMeasurement.count(
+      :conditions => ["microarray_timepoint_id = ? and measurement < ?",
+        microarray_timepoint_id,
+        measurement
+      ]
+    )
+    total = MicroarrayMeasurement.count(
+      :conditions => ['microarray_timepoint_id = ?', microarray_timepoint_id]
+    )
+    
+    return 1.0 if total == 1 #otherwise NaN's happen
+    return belows.to_f / (total-1).to_f
+  end
 end
