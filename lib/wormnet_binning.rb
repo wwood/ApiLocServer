83d4a6e6bbb2b17b4f6bc3862cfcffe44aec2b9e
Ben J Woodcroft <donttrustben near gmail.com>
2009-03-12 16:53:14 +1100

    made bin_size into a constant so it can be easily changed

--- lib/wormnet_binning.rb
+++ lib/wormnet_binning.rb
@@ -7,6 +7,7 @@ require 'array_pair'
 
 #sort wormnet results into bins based on wormnet score, set range for data bins (0,10,20..320)
 bins= []
+bin_size = 5
 
 # read in wormnet results file
 FasterCSV.foreach(ARGV[0], :headers => true, :col_sep => "\t") do |splits|
@@ -21,7 +22,7 @@ FasterCSV.foreach(ARGV[0], :headers => true, :col_sep => "\t") do |splits|
   lethal =  splits[1]
 
   # split data into bins, bins will be e.g. scores = 0, 1-10, 11-20
-  bin_number = (score/10).to_i
+  bin_number = (score/bin_size).to_i
   bins[bin_number] ||= [] #if that bin is a new bin (ie. bins[bin_number]==nil), then initialize as an empty array
   if lethal == 'true'
     bins[bin_number].push 1
@@ -33,7 +34,7 @@ end
 # Then, after reading the whole file, analyze each of the bins
 bins.each_with_index do |bin, index|
   puts [
-    "#{index*10}-#{(index+1)*10}",
+    "#{index*bin_size}-#{(index+1)*bin_size}",
     (bin.average*100).round
   ].join("\t")
 end
