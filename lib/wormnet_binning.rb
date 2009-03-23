8bcccb02a37967a656bc140bb732505e1a17c31f
Ben J Woodcroft <donttrustben near gmail.com>
2009-03-23 15:31:48 +1100

    wormnet binning now does mammalian orthologues as well

--- lib/wormnet_binning.rb
+++ lib/wormnet_binning.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 #Script to analyze and plot wormnet data
 
 require 'rubygems'
@@ -6,7 +6,8 @@ require 'fastercsv'
 require 'array_pair'
 
 #sort wormnet results into bins based on wormnet score, set range for data bins (0,10,20..320)
-bins= []
+bins1= []
+bins2= []
 bin_size = 10
 
 # read in wormnet results file
@@ -20,7 +21,7 @@ FasterCSV.foreach(ARGV[0], :headers => true, :col_sep => "\t") do |splits|
   gene_id = splits[0] #not used?
   score = splits[5].to_f.round #round off wormnet core scores to zero decimal places
   lethal =  splits[1]
-
+  mamortho = splits[8]
 
   # split data into bins, bins will be e.g. scores = 0, 1-10, 11-20
   # scores of exactly 0 are kept in a separate bin, and scores of
@@ -35,13 +36,24 @@ FasterCSV.foreach(ARGV[0], :headers => true, :col_sep => "\t") do |splits|
   else #score is >0 and not a multiple of 10
 
     bin_number = (score/bin_size).to_i+1
-  end 
+  end
 
-  bins[bin_number] ||= [] #if that bin is a new bin (ie. bins[bin_number]==nil), then initialize as an empty array
+  bins1[bin_number] ||= [] #if that bin is a new bin (ie. bins[bin_number]==nil), then initialize as an empty array
   if lethal == 'true'
-    bins[bin_number].push 1
+    bins1[bin_number].push 1
   else
-    bins[bin_number].push 0
+    bins1[bin_number].push 0
+  end
+
+  #make bins for genes without mammalian orthologues
+
+  bins2[bin_number] ||= [] #if that bin is a new bin (ie. bins[bin_number]==nil), then initialize as an empty array
+  if mamortho == 'false'
+    if lethal == 'true'
+      bins2[bin_number].push 1
+    else
+      bins2[bin_number].push 0
+    end
   end
 end
 
@@ -49,14 +61,17 @@ end
 
 #headings
 puts [
-  "Score range",
+  "Score_range",
   "No. genes",
-  "Lethal Gene",
-  "Lethal(%)"
+  "Lethal_Gene",
+  "Lethal(%)",
+  "Nomam_No._genes",
+  "Nomam_Lethal_Gene",
+  "Nomam_Lethal(%)"
 ].join("\t")
 
 
-bins.each_with_index do |bin, index|
+bins1.each_with_index do |bin1, index|
   bin_range = nil
   if index == 0
     bin_range =  0
@@ -65,17 +80,39 @@ bins.each_with_index do |bin, index|
   end
 
   #calculate no of genes per bin
-  if bin.nil?
+  if bin1.nil?
     numgen = 0; wscore = 0
   else
-    numgen = bin.length
-    wscore = (bin.average*100).round
+    numgen = bin1.length
+    wscore = (bin1.average*100).round
+  end
+
+  #calculate for genes without mammalian orthologues
+  nomamnumgen = nil
+  nomamscore = nil
+  bin2 = bins2[index]
+  bin_range = nil
+  if index == 0
+    bin_range =  0
+  else
+    bin_range = "#{((index-1)*bin_size)+1}-#{index*bin_size}"
+  end
+
+  if bin2.nil? or bin2.empty?
+    nomamnumgen = 0; nomamscore = 0
+  else
+    nomamnumgen = bin2.length
+    nomamscore = (bin2.average*100).round
   end
 
   puts [
     bin_range,
     numgen,
-    (bin.nil? ? 0 : bin.sum),
-    wscore
+    (bin1.nil? ? 0 : bin1.total),
+    wscore,
+    nomamnumgen,
+    nomamscore,
+    (bin2.nil? ? 0 : bin2.total)
+
   ].join("\t")
 end
