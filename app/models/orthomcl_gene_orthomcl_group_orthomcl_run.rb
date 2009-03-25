af09eecec4501499aaf2c924dad09a0aa3be930f
Ben J Woodcroft <donttrustben near gmail.com>
2009-03-25 16:54:59 +1100

    working on arff generator, not yet working

--- app/models/orthomcl_gene_orthomcl_group_orthomcl_run.rb
+++ app/models/orthomcl_gene_orthomcl_group_orthomcl_run.rb
@@ -5,7 +5,7 @@ class OrthomclGeneOrthomclGroupOrthomclRun < ActiveRecord::Base
 
   # Find or create using the usual ids
   def self.find_or_create_me(orthomcl_gene_id, orthomcl_group_id, orthomcl_run_id)
-    self.find_all_by_orthomcl_gene_id_and_orthomcl_group_id_and_orthomcl_run_id(
+    self.find_or_create_by_orthomcl_gene_id_and_orthomcl_group_id_and_orthomcl_run_id(
       orthomcl_gene_id, orthomcl_group_id, orthomcl_run_id
     )
   end
