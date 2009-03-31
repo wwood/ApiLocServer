cc41ac805940977f4da51308954086f5f34e3708
Ben J Woodcroft <donttrustben near gmail.com>
2009-03-31 17:28:47 +1100

    worked on transmembrane domains, and moved some scripts to ben_bioinformatics plugin

--- lib/phobius_wrapper.rb
+++ lib/phobius_wrapper.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'transmembrane'
 include Transmembrane
 require 'tempfile'
@@ -22,9 +24,9 @@ class PhobiusWrapper
             Tempfile.open('phobiuserr') {|err|
               result = system("phobius -short #{tempfilein.path} >#{out.path} 2>#{err.path}")
         
-              if !result
-                raise Exception, "Running phobius program failed. See $? for details."
-              end
+#              if !result
+#                raise Exception, "Running phobius program failed. $? was #{$?.inspect}"
+#              end
         
               # make sure the error file isn't anything unexpected
               # by default it prints a copyright notice thing
@@ -96,5 +98,38 @@ class PhobiusResult
     protein.push tmd
     
     return protein
+  end
+end
+
+
+
+# If being run directly instead of being require'd,
+# output one transmembrane per line, and
+# indicate that a particular protein has no transmembrane domain
+if $0 == __FILE__
+  require 'bio'
+
+  runner = PhobiusWrapper.new
+
+  Bio::FlatFile.auto(ARGF).each do |seq|
+    result = runner.calculate(seq.seq)
+    name = seq.definition
+
+    if result.has_domain?
+      # At least one TMD found. Output each on a separate line
+      result.transmembrane_domains.each do |tmd|
+        puts [
+          name,
+          'Transmembrane',
+          tmd.start,
+          tmd.stop
+        ].join("\t")
+      end
+    else
+      puts [
+        name,
+        'No Transmembrane Domain Found'
+      ].join("\t")
+    end
   end
 end
