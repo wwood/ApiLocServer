ac7a72423541c6f420dfa1588070e1dc32b50d6d
Ben J Woodcroft <donttrustben near gmail.com>
2009-10-08 12:46:44 +1100

    loc stuff

--- app/models/localisation_modifier.rb
+++ app/models/localisation_modifier.rb
@@ -2,7 +2,7 @@ class LocalisationModifier < ActiveRecord::Base
   KNOWN_MODIFIERS = %w(
     weak sometimes some little sporadically reduced variable partially
     strong substantial mainly throughout predominantly bright
-    punctate homogenous homogenously diffuse uniform heterogenous
+    punctate homogenous homogenously diffuse uniform heterogenous entire
     semi-punctate smooth beaded patchy
   )
 
