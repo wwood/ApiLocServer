0158d5737aa1d8fd9597ab8358f743843dc382f4
Ben J Woodcroft <donttrustben near gmail.com>
2009-10-12 08:36:00 +1100

    moved to orthomcl version 3 and refactored the orthomcl code into its own file

--- app/models/orthomcl_run.rb
+++ app/models/orthomcl_run.rb
@@ -2,13 +2,19 @@ class OrthomclRun < ActiveRecord::Base
   has_many :orthomcl_gene_orthomcl_group_orthomcl_runs, :dependent => :destroy
   has_many :orthomcl_genes, :through => :orthomcl_gene_orthomcl_group_orthomcl_runs, :dependent => :destroy
   has_one :orthomcl_groups, :through => :orthomcl_gene_orthomcl_group_orthomcl_runs, :dependent => :destroy
+
+  ORTHOMCL_OFFICIAL_VERSION_3_NAME = 'Official OrthoMCL v3'
+
+  def self.official_run_v3
+    OrthomclRun.find_or_create_by_name(ORTHOMCL_OFFICIAL_VERSION_3_NAME)
+  end
   
   def self.official_run_v2_name
     'Official OrthoMCL v2'
   end
   
   def self.official_run_v2
-    return OrthomclRun.find_or_create_by_name(official_run_v2_name)
+    OrthomclRun.find_or_create_by_name(official_run_v2_name)
   end
   
   def self.seven_species_no_filtering_name
