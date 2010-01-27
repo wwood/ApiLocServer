a711911cbbea72dd721ffacdb58598b3fac74c09
Ben J Woodcroft <donttrustben near gmail.com>
2010-01-27 13:09:50 +1100

    streamlining uploading of apiloc from scratch

--- lib/modules/eu_path_db.rb
+++ lib/modules/eu_path_db.rb
@@ -227,10 +227,46 @@ class BScript
 
 
   def upload_apiloc_from_scratch
+    go_to_database
+    download_uniprot_data
+    uniprot_to_database
+    orthomcl_to_database
+    upload_apiloc_from_scratch
+
     # Upload basic gene identifiers
     upload_apiloc_gffs
     upload_gondii_gene_table_to_database
     upload_apiloc_fasta_files
+
+    upload_proteomic_data
+
+    tetrahymena_orf_names_to_database
+    tetrahymena_gene_aliases_to_database
+    yeastgenome_ids_to_database
+    elegans_wormbase_identifiers
+    uniprot_ensembl_databases
+    uniprot_refseq_databases
+    chlamydomonas_link_to_orthomcl_ids
+
+    update_known_four_letters
+    OrthomclGene.new.link_orthomcl_and_coding_regions(
+      "hsap mmus scer drer osat crei atha dmel cele",
+      :accept_multiple_coding_regions => true
+    )
+    OrthomclGene.new.link_orthomcl_and_coding_regions(
+      Species::APICOMPLEXAN_NAMES.reject{|a|
+        a == Species::BABESIA_BOVIS_NAME
+      }.collect { |a|
+        Species.find_by_name(a).orthomcl_three_letter
+      }
+    )
+
+    LocalisationSpreadsheet.new.upload
+    proteomics_to_database
+    Publication.fill_in_all_extras! #has to be after spreadsheet and proteomics, because they provide the pubmed ids to expand on
+
+    DevelopmentalStageTopLevelDevelopmentalStage.new.upload_apiloc_top_level_developmental_stages
+    ApilocLocalisationTopLevelLocalisation.new.upload_apiloc_top_level_localisations
   end
 
   def upload_apiloc_gffs
