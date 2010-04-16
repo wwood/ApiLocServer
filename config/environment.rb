9c361a75da8c5eee4da77795c183a9156aca6790
Ben J Woodcroft <donttrustben near gmail.com>
2010-04-16 12:26:15 +1000

    Revert "merging, removing memorylogic include from application controller which is a development-specific thing"
    This reverts commit edb069be4a1dafd943606a3b61ff654a37ca4e68, reversing
    changes made to 2ac49e8c1ae59602bb27ac0003981bb37c53a62d.

--- config/environment.rb
+++ config/environment.rb
@@ -89,5 +89,3 @@ class FlorianApicoplastTransmembraneDomain < TransmembraneDomain; end
 class FlorianMitochondriaTransmembraneDomain < TransmembraneDomain; end
 class FlorianPlasmaMembraneAndAlikeTransmembraneDomain < TransmembraneDomain; end
 class FlorianIntracellularTransmembraneDomain < TransmembraneDomain; end
-
-ExceptionNotifier.exception_recipients = %w(ben@localhost b.woodcroft@pgrad.unimelb.edu.au)
